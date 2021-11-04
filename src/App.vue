a92ccb0ef9ce17889702d40048f54f4193a24fb0
nazimboudeffa <boudeffa@gmail.com>
2021-11-04 23:45:29 +0100

    Added franch translation rights

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <header class="header">
       <div class="container">
-      <router-link to="/" class="header__link">Cyberpunk Red Character Generator</router-link>
+      <router-link to="/" class="header__link">Cyberpunk Red Générateur de Personnage</router-link>
       </div>
     </header>
 
@@ -15,10 +15,11 @@
         <p>
           Content &copy; R. Talsorian Games, Inc.<br />
           Code &copy; Ben Serrette
+          French translation Nazim Boudeffa
         </p>
       </div>
       <div class="footer__content">
-        <a href="https://github.com/benabus/cyberpunk-red-character-generator">Contribute on Github</a>
+        <a href="https://github.com/benabus/cyberpunk-red-character-generator">Contribuer sur Github</a>
       </div>
     </footer>
   </div>
